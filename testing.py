5834b9d48a50dd3cda36434e5e67e8bd9cb0920b
gasolina90 <moralerick90@gmail.com>
2023-05-16 10:23:09 -0600

    modified file

--- testing.py
+++ testing.py
@@ -6,130 +6,57 @@ Organizing user data from users.json
 import json
 
 # Global variables
-email_list = []
-username_list = []
-snapchat_list = []
-interests_list = []
-userid_list = []
+# email_list = []
+# username_list = []
+# snapchat_list = []
+# interests_list = []
+# userid_list = []
 
-with open(r"users.json") as file:
-    data_dict = {}
-    interests_dict = {}
+# with open(r"users.json") as file:
+#     data_dict = {}
+#     interests_dict = {}
 
-    data_dict = json.load(file)
-    # print(data_dict)
-    for i in data_dict:
-        email_list.append(i["userEmail"])
-        username_list.append(i["userName"])
-        snapchat_list.append(i["snapInfo"])
-        interests_list.append(i["userInterests"])
-        userid_list.append(i["userID"])
+#     data_dict = json.load(file)
+#     # print(data_dict)
+#     for i in data_dict:
+#         email_list.append(i["userEmail"])
+#         username_list.append(i["userName"])
+#         snapchat_list.append(i["snapInfo"])
+#         interests_list.append(i["userInterests"])
+#         userid_list.append(i["userID"])
 
-for i in interests_list:
-    print(i)
-
-    
+# for i in interests_list:
+#     print(i)
     # interests_dict = json.loads(interests_list)
     # print(interests_dict)
 
 '''==========================================================================================================================='''
 
 '''
-# Messing around with PCA (partial component analysis)
-# '''
+Visualizing data with pandas and matplotlib
+'''
 
-import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from sklearn.datasets import load_iris
 
-iris = load_iris()
-# Display the feature names and Encoding of target vars
-print(f"These are the feature names\n{iris.feature_names}\n")
-print(f"These are the target names\n{iris.target_names}\n")
+# Open and parse the json file
+with open(r"userInsterest.json") as file:
+    data_df = pd.read_json(file)
 
-# Format the data from iris dataset to a data frame from pandas
-data = pd.DataFrame(iris.data)
-data.columns = iris.feature_names
-data["CLASS"] = iris.target
-print(f"These are the header titles\n{data.head()}\n")
+    # Set the userID values as indicies
+    data_df.set_index("userID", inplace=True)
+    data_df = data_df["userInterests"].apply(pd.Series).T
+    # # Convert the data_df values to ints and sort them by numerical order
+    # data_df.columns = data_df.columns.str[1:].astype(int)
+    # data_df = data_df.sort_index(axis=1)
 
-# Display the range index, all data columns, datatypes, and memory used with info() method
-print(data.info())
-# Display a statistical overview of the dataset
-print(data.describe())
+    # Set plot type, and axes lables
+    ax = data_df.plot(kind="bar", rot=0)
+    ax.set_xlabel("Question Number")
+    ax.set_ylabel("Interest Level")
+    ax.legend(title="user ID", bbox_to_anchor=(1,1))
 
-# Split the data, specifically the features and targets as 2 separate variables
-x = data.iloc[:,:4]     # Features
-y = data.iloc[:,4]      # Targets
-print(f"This is the shape of x and y. Format = (rows, features)\n{x.shape, y.shape}")
-print("Result is a tuple, or tuple of tuples\n")
+    plt.show()
 
 '''==========================================================================================================================='''
 
-'''
-Actually using PCA from sklearn
-'''
-
-from sklearn.decomposition import PCA
-
-# Instantiate a new object of PCA class
-pca = PCA(n_components=4)       # The shapes arguments need to match when the error is thrown
-# Changes the n_components=3 to see the error Im talking about
-# Perform PCA, fit the PCA model to the input data and transform it to the principal components space (column)
-X = pca.fit_transform(x)
-# 
-print(f"Covariance matrix on how much the features vary together\n{pca.get_covariance()}\n")
-explained_variance = pca.explained_variance_ratio_
-print(f"Total variance ratios for each principal component\n{explained_variance}\n")
-
-print("="*50)
-
-# Visualize the covariance to help select the amount of principal components
-with plt.style.context("dark_background"):
-    plt.figure(figsize=(6,4))
-    plt.bar(range(4), explained_variance, alpha=0.5, align="center", label="individual explained variance")
-    plt.ylabel("Explained variance ratio")
-    plt.xlabel("Principal components")
-    plt.legend(loc="best")
-    plt.tight_layout()
-    # plt.show()      # Use plt.show() to show the plot you just created
-
-print()
-
-# Seeing the plot, we can confidently conclude that there are 4 components with  variance
-# We therefore go back to where we created `pca` variable, line 75, set the n_components= argument to however many components have significant variance
-
-'''==========================================================================================================================='''
-
-'''
-Splitting the data into training and test sets
-'''
-# Why do this?
-# To prevent predictions on only trained data leading to overfitting, thus giving bad results for unknown data
-from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=20, stratify=y)
-
-'''==========================================================================================================================='''
-
-'''
-Training the Model and predicting
-'''
-
-from sklearn.neighbors import KNeighborsClassifier
-
-# Save the KNN classifier to a variable
-model = KNeighborsClassifier(7)
-# Fit the training data to the model
-model.fit(X_train, y_train)
-# Predict the class labels of the test dataset
-y_pred = model.predict(X_test)
-
-from sklearn.metrics import confusion_matrix
-from sklearn.metrics import accuracy_score
-
-cm = confusion_matrix(y_test, y_pred)
-# Confusion matrix will who the count all false positives and negatives, and true positives and negatives
-print(f"The confusion matrix\n{cm}\n")
-# Accuracy score is formatted as a percentage (0.5 = 50%) as how effective the prediction is for predicting new data
-print(f"The accurcay score\n{accuracy_score(y_test, y_pred)}")
