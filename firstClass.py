498011a34a4ed429e78a69ca7795866703cb7df7
gasolina90 <moralerick90@gmail.com>
2023-06-22 15:03:14 -0600

    modified file

--- firstClass.py
+++ firstClass.py
@@ -10,12 +10,15 @@
 # ALL CLASS STRUCTURES ARE SUBJECT TO CHANGE
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
-from users_copy import dataset
 import numpy as np
 import json
 import math
 
 class User:
+    '''
+    A class to organize and choose which user's data will be returned. There will be one member attribute: ID_intr. There will be one member method assign_data() to reorganize and elect which user's data will be returned.
+    ID_intr, stands for ID and Interests, will save the extracted data to its own variable. It will then be passed in as a parameter to choose which specific users to eventually return and use in the custom Math class.
+    '''
     def __init__(self):
         self.ID_intr = {}
 
@@ -24,11 +27,17 @@ class User:
         pass            
 
 class Math:
+    '''
+    A class to handle user data and calculate similarity values. 4 member attributes: user1_data, user2_data as dictionaries, and d, r as floating point integers, and 2 member methods: euclid_distance(), and pearson_coeffictient().
+    Where user1_data and user2_data will be passed in as parameters, d is Euclidean distance between 2 points and r is Pearson's correlation coefficient will be calculated from within the class structure.
+    '''
+
+    # Pass in data as parameters as data?
     def __init__(self):
         self.user1_data = {}
         self.user2_data = {}
-        self.d = 0
-        self.r = 0
+        self.d = 0.0
+        self.r = 0.0
 
     def euclid_distance(self):
         self.d = math.sqrt(sum(pow(a - b, 2) for a, b in zip(self.user1_data, self.user2_data)))
@@ -56,6 +65,10 @@ class Math:
         return self.r
 
 class Data:
+    '''
+    A class to extract all necessary user data from a JSON file. There will be 3 member attributes: interests, ids, data_dict. There will be 1 member method: process_data().
+    interests and ids will be used as variables to be assigned the original userInterests and userID data from the JSON. Where data_dict will be intialized as a blank dictionary and be used to save the necessary data with the userIDs as the keys, and userInterests as an embedded dictionary of keys and values.
+    '''
     def __init__(self):
         self.interests = None
         self.ids = None
@@ -63,14 +76,11 @@ class Data:
 
     def process_data(self):
         with open(r"users.json") as file:
-
+            # Load the file from Firebase as a json object
             profile_json = json.load(file)
-            # print(profile_json[0])
 
+            # Loop through 
             for profile in profile_json:
-                # print(profile["userID"])
-                # print(profile["userInterests"])
-
                 # Assign the original userID and userInterests data to temporary variables
                 self.ids = profile["userID"]
                 self.interests = profile["userInterests"]
@@ -82,12 +92,11 @@ class Data:
         
 
 def main():
-    # figure out which parameters to pass in to which objects
-    # this order will probably be best
     D = Data()   # Extract all necessary user data, to pass to User()
     U = User()   # reorganize data and choose users, to pass to Math()
     M = Math()   # Calculate the distance (d) and pearson coefficient (r) for the users chosen
 
+    # P.S. This program can be run as is if you would like to see the extracted data for yourself ^_^
     D.process_data()
     extracted_data = D.process_data()
     print(extracted_data)
